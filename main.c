410350ad8575b05f3f73b0bbf76b4d24e2381f9c
silasb <silas.baronda@gmail.com>
2011-01-29 15:24:00 -0500

    commit to transfer

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ typedef struct
   GtkWidget *window;
   GtkWidget *urlBarField;
   GtkWidget *responseField;
+  GtkComboBox *methodType;
+  GtkWidget *data;
 } Window;
 
 struct MemoryStruct {
@@ -55,12 +57,22 @@ on_SendButton_clicked(GtkObject *object, Window *window)
   CURL *curl;
   CURLcode res;
   GtkTextBuffer *buffer;
-  const gchar *url;
+  GtkTextBuffer *dataBuffer;
+  const gchar *url, *data2;
+  gchar *postType=NULL;
+  GtkTreeIter iter, start, end;
+  GtkTreeModel *model;
 
   struct MemoryStruct chunk;
 
   chunk.memory=NULL;
   chunk.size=0;
+
+  if( gtk_combo_box_get_active_iter( window->methodType, &iter ) )
+  {
+    model = gtk_combo_box_get_model( window->methodType);
+    gtk_tree_model_get( model, &iter, 0, &postType, -1 );
+  }
 
   curl_global_init(CURL_GLOBAL_ALL);
 
@@ -68,7 +80,19 @@ on_SendButton_clicked(GtkObject *object, Window *window)
 
   if(curl) {
     url = gtk_entry_get_text(GTK_ENTRY(window->urlBarField));
+    dataBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(window->data));
+    gtk_text_buffer_get_start_iter(dataBuffer, &start);
+    gtk_text_buffer_get_end_iter(dataBuffer, &end);
+    data2 = gtk_text_buffer_get_text(dataBuffer, &start, &end, FALSE);
+
     curl_easy_setopt(curl, CURLOPT_URL, url);
+
+    if(g_strcmp0(postType, "POST") == 0) {
+      printf("%s\n", data2);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data2);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)strlen(data2));
+    }
+
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
@@ -85,6 +109,34 @@ on_SendButton_clicked(GtkObject *object, Window *window)
   }
 
   curl_global_cleanup();
+}
+
+void
+populate_combobox( GtkComboBox *combo )
+{
+  GtkListStore *store;
+  GtkTreeIter iter, first_item;
+  GtkCellRenderer *cell;
+
+  store = gtk_list_store_new( 1, G_TYPE_STRING );
+
+  gtk_list_store_append( store, &iter );
+  first_item = iter;
+  gtk_list_store_set( store, &iter, 0, "GET", -1 );
+  gtk_list_store_append( store, &iter );
+  gtk_list_store_set( store, &iter, 0, "POST", -1 );
+
+  gtk_combo_box_set_model( combo, GTK_TREE_MODEL( store ));
+  gtk_combo_box_set_active_iter( combo, &first_item );
+  //window->methodType = gtk_combo_box_new_with_model( GTK_TREE_MODEL( store ) );
+  g_object_unref( G_OBJECT( store ) );
+  //gtk_combo_box_append_text( combo, "hello world" );
+  cell = gtk_cell_renderer_text_new();
+
+  gtk_cell_layout_pack_start( GTK_CELL_LAYOUT( combo ), cell, TRUE );
+
+  gtk_cell_layout_set_attributes( GTK_CELL_LAYOUT( combo ), cell, "text", 0, NULL );
+
 }
 
 int
@@ -104,6 +156,10 @@ main(int argc, char *argv[])
   window->window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
   window->urlBarField = GTK_WIDGET(gtk_builder_get_object(builder, "URLBarField"));
   window->responseField = GTK_WIDGET(gtk_builder_get_object(builder, "ResponseField"));
+  window->data = GTK_WIDGET(gtk_builder_get_object(builder, "data"));
+  window->methodType = GTK_COMBO_BOX(gtk_builder_get_object(builder, "methodType"));
+
+  populate_combobox( window->methodType );
 
   gtk_builder_connect_signals(builder, window);
 
